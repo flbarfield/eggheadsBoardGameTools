6c34ce3e9a190bb0b8e568bbd5f02372165689fe
Ricky Barfield <barfieldfredrick@gmail.com>
2023-10-20 06:31:19 -0700

    added some images. Planned the app with comments. Tinkered a little with premade tailwind components but decided I didnt like them

--- eggheads-board-game-tools/src/app/page.js
+++ eggheads-board-game-tools/src/app/page.js
@@ -1,9 +1,33 @@
-import Header from '../Components/Header'
+import Navbar from '../Components/Navbar'
 
+
+// TO DOs:
+
+// ---- TTRPG Section ----
+//    Quick References
+//      Status Affects
+//    Monster Creator
+//    Battle Section
+//      Initiative/turn tracker
+//    Character Creator
+//    Name Generators for towns/cities/characters
+//    DM Randomizors
+//      Shops
+//      Loot
+//      Wild Magic Surge
+//      Random Encounters
+//      Quick Maps
+//
+// ---- Board Game Section ----
+//    Add your current BGs to a list to see what you have
+//      Randomly can choose one if players can't choose what to play
+//
+// ---- Miniature Section ----
+//    Be able to upload your image of a terrain piece...Comes back with where to buy similar terrain pieces
 export default function Home() {
   return (
-    <main>
-      <Header/>
+    <main className='w-screen h-screen bg-blue-700'>
+      <Navbar/>
     </main>
   )
 }
