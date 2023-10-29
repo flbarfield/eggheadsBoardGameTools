75a1a9ed06ed68b0bdab8d1c87c7eda19374cc65
Ricky Barfield <barfieldfredrick@gmail.com>
2023-10-29 14:29:44 -0700

    good enough for now nav bar work

--- eggheads-board-game-tools/src/Components/Navbar.jsx
+++ eggheads-board-game-tools/src/Components/Navbar.jsx
@@ -1,9 +1,21 @@
+'use client'
+
 import logo from '../../public/Images/tabIcon.png'
 import Image from 'next/image'
 import Link from 'next/link'
 import NavbarLink from './NavbarLink'
 
 const Navbar = () => {
+
+    function toggleNavLinks(e) {
+        e.target.nextElementSibling.classList.toggle('hidden')
+    }
+
+    // function toggleLinksFromUL(e) {
+    //     alert(e.target)
+    //     // e.target.classList.toggle('hidden')
+    // }
+
     return (
         <nav> 
             <div>
@@ -16,19 +28,24 @@ const Navbar = () => {
                         </Link>
                     </li>
                     <li>
-                        <span className='hover:bg-EggheadBlue hover:text-white p-2 rounded-md cursor-pointer relative'>Table Top Roleplay</span>
-                        <ul className='shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none hidden'>
+                        <span onClick={toggleNavLinks} className='hover:bg-EggheadBlue hover:text-white p-2 rounded-md cursor-pointer'>Table Top Roleplay</span>
+                        <ul className='shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none hidden absolute'>
                             <NavbarLink path='/tabletop/DMsTools' linkName='DMs Tools'/>
                             <NavbarLink path='/tabletop/playersCorner' linkName="Player's Corner"/>
                             <NavbarLink path='/tabletop/nameGenerators' linkName='Name Generators'/>
                         </ul>
-                        
                     </li>
                     <li>
-                        <span className='hover:bg-EggheadBlue hover:text-white p-2 rounded-md cursor-pointer relative'>Board Games</span>
+                        <span onClick={toggleNavLinks} className='hover:bg-EggheadBlue hover:text-white p-2 rounded-md cursor-pointer relative'>Board Games</span>
+                        <ul className='shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none hidden absolute'>
+                            <li>Not Yet Implemented.</li>
+                        </ul>
                     </li>
                     <li>
-                        <span className='hover:bg-EggheadBlue hover:text-white p-2 rounded-md cursor-pointer relative'>Miniatures</span>
+                        <span onClick={toggleNavLinks} className='hover:bg-EggheadBlue hover:text-white p-2 rounded-md cursor-pointer relative'>Miniatures</span>
+                        <ul className='shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none hidden absolute'>
+                            <li>Not Yet Implemented</li>
+                        </ul>
                     </li>
                 </ul>
             </div>
--- eggheads-board-game-tools/src/Components/NavbarLink.jsx
+++ eggheads-board-game-tools/src/Components/NavbarLink.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function NavbarLink ({path, linkName}) {
     return (
         <li className='hover:bg-EggheadBlue hover:text-white cursor:pointer'>
-            <Link  href={path}> {linkName}</Link>
+            <Link href={path}> {linkName}</Link>
         </li>
     )
 }
