4ead60b3d89689851357bb9892a0444dfaa76d14
Ricky Barfield <barfieldfredrick@gmail.com>
2023-11-03 07:10:22 -0700

    some progress with fetching before needing to get ready for work. May need useEffect

--- eggheads-board-game-tools/src/Components/PageLogic/QuickRef.jsx
+++ eggheads-board-game-tools/src/Components/PageLogic/QuickRef.jsx
@@ -10,35 +10,49 @@ import { useEffect, useState } from 'react'
 // https://www.dnd5eapi.co/api/conditions/blinded - displays condition desc
 
 export default function QuickRef () {
-    // let [data, setData] = useState('')
+    let [generatedInfoButtons, setGeneratedInfoButtons] = useState('')
+    let [generatedInfo, setGeneratedInfo] = useState('')
 
-    // async function populateAbilityData() {
-    //     try {
-    //         let res = await fetch('https://www.dnd5eapi.co/api/ability-scores')
-    //         let data = await res.json()         
-    //         alert(setData(data.results))
+    async function populateAbilityData() {
+        try {
+            const res = await fetch('https://www.dnd5eapi.co/api/ability-scores')
+            const data = await res.json()
+            
+            // console.log(data.results[0].name)
+            let i = 0
+            while (i < data.count) {
+                setGeneratedInfoButtons += ' ' + data.results[i].name
+                i++
+            }
+            console.log(setGeneratedInfoButtons) 
+            
    
-    //     } catch (error) {
-    //         console.log('Error retriving data. May be an issue with the API')
-    //     }
-    // }
+        } catch (error) {
+            alert('Error retriving data. May be an issue with the API')
+        }
+    }
 
     return(
         <section id='dmContent' className='flex flex-col gap-5 px-12 lg:basis-1/2 mb-10'>
             <h1>Quick Reference</h1>
             <Image className='rounded-md mx-auto' src={AfflictionsImg} width={800} height={500} placeholder='blur' alt="Affliction Warlock" />
-            <section className='flex flex-wrap gap-3'>
-                <Button label='Ability Scores'/>
-                <Button label='Classes'/>
-                <Button label='Conditions'/>
-                <Button label='Feats'/>
-                <Button label='Languages'/>
-                <Button label='Skills'/>
-                <Button label='Rules'/>    
-                <Button label='Traits'/>  
+            <section>
+                <ul className='flex flex-wrap gap-3'>
+                    <li onClick={populateAbilityData}><Button label='Ability Scores'/></li>
+                    <li><Button label='Classes'/></li>
+                    <li><Button label='Conditions'/></li>
+                    <li><Button label='Feats'/></li>
+                    <li><Button label='Languages'/></li>
+                    <li><Button label='Skills'/></li>
+                    <li><Button label='Rules'/></li>
+                    <li><Button label='Traits'/></li>
+                </ul>
             </section>
             <section id='generatedInfo'>
-
+                <ul>
+                    <p>{generatedInfoButtons}</p>
+                </ul>
+                {generatedInfo}
             </section>
             
             <p className='text-center text-red-500'>Not Yet Implemented.</p>
