69d3f018b5db97731c429549d101ee5808d8db01
Ricky Barfield <barfieldfredrick@gmail.com>
2023-11-08 09:06:21 -0700

    slowly implementing combat section. Switching computers before this one dies

--- eggheads-board-game-tools/src/Components/PageLogic/CombatTracker.jsx
+++ eggheads-board-game-tools/src/Components/PageLogic/CombatTracker.jsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import CombatImg from '../../../public/Images/Combat.jpeg'
+import Button from '../Button'
+import { useState } from 'react'
 
 //    Battle Section
 //      Initiative/turn tracker
@@ -7,12 +9,48 @@ import CombatImg from '../../../public/Images/Combat.jpeg'
 
 
 export default function CombatTracker () {
+    let [currentMonsterList, setCurrentMonsterList] = useState([])
+    
+    async function populateMonsters() {
+        try {
+            const res = await fetch('https://www.dnd5eapi.co/api/monsters')
+            const data = await res.json()
+
+            let count = 0
+            let monsterList = []
+
+            // (count < data.count) , when I'm running this for real.
+            while (count < 5){
+                monsterList.append.push(data.results[count].name)
+                count++
+            }
+            console.log(monsterList)
+            setCurrentMonsterList(monsterList)
+
+            console.log(currentMonsterList)
+
+        } catch (error) {
+            console.log('Error Populating Monsters. Problem with API')
+        }
+    }
+
+    populateMonsters()
+
     return(
         <section id='dmContent' className='flex flex-col gap-5 px-12 lg:basis-1/2 mb-10'>
             <h1>Combat Tracker</h1>
             <Image className='rounded-md mx-auto' src={CombatImg} width={800} height={500} placeholder='blur' alt="Characters Fighting" />
-            <section>
-                <p className='text-center text-red-500'>Not Yet Implemented.</p>
+            <section className='flex flex-col mx-auto'>
+                <div>
+                    <i className='fa-solid fa-magnifying-glass mr-5'></i>
+                    <input className='px-2' type="text" placeholder='Monster Search'/>
+                    <Button label={'Add'}/>
+                </div>
+                <ul className='mh-50 overflow-scroll'>
+                    {currentMonsterList}
+                    <li><span>test</span></li>
+                    <li><span>test</span></li>
+                </ul>
             </section>
         </section>
     )
