41587c50a304680dd8a486f63646e70fe957c854
Ricky Barfield <barfieldfredrick@gmail.com>
2023-11-18 18:48:38 -0700

    found a ton of CSV files that hold a bunch of different loot properties thatd be greaat for custom items! Going to figure out how to parse and use these for sure

--- eggheads-board-game-tools/src/Components/TextPill.jsx
+++ eggheads-board-game-tools/src/Components/TextPill.jsx
@@ -1,7 +1,7 @@
 export default function TextPill ({header, date, desc1, desc2, desc3}) {
     return (
     <>
-        <section className='overflow-scroll overflow-x-hidden h-52 px-10 py-5 bg-EggheadBlue max-w-sm mx-auto rounded-lg text-white font-medium mb-5'>
+        <section className='overflow-scroll overflow-x-hidden max-h-52 px-10 py-5 bg-EggheadBlue max-w-sm mx-auto rounded-lg text-white font-medium mb-5'>
             <span>{header}<br/></span>
             <span>{date}</span>
             <p className='mt-5 border-t-2 pt-4 text-ellipsis'>{desc1} <br /> <br /> {desc2} <br /> <br /> {desc3} </p>
