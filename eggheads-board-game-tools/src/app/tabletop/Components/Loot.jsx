05ff2f10e585e527dbb786417f418101af19a74d
Ricky Barfield <barfieldfredrick@gmail.com>
2023-11-22 20:09:15 -0700

    getting ready to add logic to loot section

--- eggheads-board-game-tools/src/app/tabletop/Components/Loot.jsx
+++ eggheads-board-game-tools/src/app/tabletop/Components/Loot.jsx
@@ -1,12 +1,32 @@
 import Image from 'next/image'
 import LootImg from '../../../../public/Images/treasure.webp'
+import Button from '@/Components/Button'
+import Papa from 'papaparse'
+import NAButton from '@/Components/NAButton'
+
 
 export default function Loot () {
+    let data = Papa.parse('../../../../src/csv/pickpocketLoot.csv')
+    console.log(data)
+
     return(
         <section id='dmContent' className='flex flex-col gap-5 px-12 lg:basis-1/2 mb-10'>
             <h1>Loot</h1>
-            <Image className='rounded-md mx-auto' src={LootImg} width={800} height={300} placeholder='blur' alt="Treasure box" />
-            <p className='text-center text-red-500'>Not Yet Implemented.</p>
+                <Image className='rounded-md mx-auto' src={LootImg} height={350} placeholder='blur' alt="Treasure box" />
+                <ul className='flex flex-wrap gap-2'>
+                    <li><Button label={'Pickpocket Loot'}/></li>
+                    <li><NAButton label={'Individual Treasure CR 0-4'}/></li>
+                    <li><NAButton label={'Individual Treasure CR 5-10'}/></li>
+                    <li><NAButton label={'Individual Treasure CR 11-16'}/></li>
+                    <li><NAButton label={'Individual Treasure CR 17+'}/></li>
+
+                </ul>
+                <ul className='flex flex-wrap gap-2 mt-5'>
+                    <li><NAButton label={'Treasure Hoard CR 0-4'}/></li>
+                    <li><NAButton label={'Treasure Hoard CR 5-10'}/></li>
+                    <li><NAButton label={'Treasure Hoard CR 11-16'}/></li>
+                    <li><NAButton label={'Treasure Hoard CR 17+'}/></li>
+                </ul>   
         </section>
     )
 }
